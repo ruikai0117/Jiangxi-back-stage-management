0c7e8ae8d0e06a7d836884eb5cf6832950735064
双面胶 <940165601@qq.com>
2021-05-24 15:55:11 +0800

    sync.native.组件v-model,通讯

--- src/components/Home/home-test.vue
+++ src/components/Home/home-test.vue
@@ -1,35 +1,54 @@
 <!--
  * @Author: Hou Ruikai
  * @Date: 2021-04-15 10:28:25
- * @LastEditTime: 2021-04-15 15:01:49
+ * @LastEditTime: 2021-05-24 15:13:40
  * @LastEditors: Please set LastEditors
  * @Description:测试模型-项目级-通用组件
  * @FilePath: /测试/Jiangxi-back-stage-management/src/components/Home/home.vue
 -->
 <template>
-    <div>
-        <p @click="Push">
- $one,测试是否可以获取子组件中的数据
-        </p>
+  <div>
+    <p @click="Push">$one,测试是否可以获取子组件中的数据</p>
+    <!-- 绑定一个名为‘Push’的方法做事件发射，发射的事件名为‘NumberSize’，并且传递付组件所需要的值。 -->
+    <input
+      type="text"
+      :value="VModelKey"
+      @input="$emit('input', $event.target.value)"
+    />
+    <!--:value即是让该输入框的值恒等于它所绑定的值，所以 :value="VModelKey"，此时VModelKey该值为夫组件传递过来的。
+    oninput事件在用户输入时触发，它是在元素值发生变化时立即触发；利用input原生属性的特性在该事件上做名为‘input’的事件发射操作，并且传递的参数为此时当前该输入框的值
+    ($event.target.value)，
+    父组件中再对该值做处理利用prop传递过来，v-model此时便做了隐式处理 -->
+    <div v-if="show">
+      <p>默认初始值是{{ show }}，所以是显示的</p>
+      <button @click.stop="closeDiv">关闭</button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {    
-        name:'home-test',
-        data() {
-            return {
-                HomeArray:1,
-            }
-        },
-        methods: {
-            Push(){
-                this.$emit("NumberSize",Math.round(Math.random()*10))
-            }
-        }
+export default {
+  name: "home-test",
+  props: {
+    VModelKey: String,
+    show:Boolean
+  },
+  data() {
+    return {
+      HomeArray: 1,
+    };
+  },
+  methods: {
+    Push() {
+      this.$emit("NumberSize", Math.round(Math.random() * 10));
+    },
+    closeDiv(){
+        this.$emit('update:show', false); //触发 input 事件，并传入新值
     }
+  },
+  mounted() {},
+};
 </script>
 
 <style  scoped>
-
 </style>
--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Hou Ruikai
  * @Date: 2021-04-15 10:21:11
- * @LastEditTime: 2021-04-15 15:02:08
+ * @LastEditTime: 2021-05-24 15:51:31
  * @LastEditors: Please set LastEditors
  * @Description: 测试模型 view为页面
  * @FilePath: /测试/Jiangxi-back-stage-management/src/view/Home/index.vue
@@ -10,9 +10,32 @@
   <div>
     <div class="TestGetChildren">
       <h3>hello，world</h3>
-      <home-test @NumberSize="GetChildren" />
+      <home-test
+        :show.sync="IsShow"
+        @click.native="Native"
+        @NumberSize="GetChildren"
+        v-model="VModelKey"
+      />
+      <!-- 
+        * @click.native="Native" native修饰符是用来在自定义组件上绑定原生事件的
+        * @NumberSize="GetChildren" NumberSize是子组件发射的自定义事件，用于子传父，组件传值
+        * v-model="VModelKey" 在自定义组件中使用v-model实现双向绑定. 
+        需要子组件中对input标签value熟悉做处理 并绑定input事件来进行事件发射以来达到父子组件的双向绑定
+        详情见home-test组件中
+
+        * :show.sync="IsShow""其实是语法糖。是其一种简写形式。 
+        在这里它省略了一个方法。this.IsShow = false。并且省略了一个在自定义组件上事件绑定的过程
+       -->
 
       <div class="test-is-show" v-if="IsShow">test-is-show</div>
+      <div>v-model:{{ VModelKey }}</div>
+      <button @click="changeValue">toggle</button>
+      <!-- 用于测试.sync的作用 -->
+
+      <button v-on:click="TransitonShow = !TransitonShow">transition显示隐藏</button>
+      <transition name="fade">
+        <p v-if="TransitonShow">hello</p>
+      </transition>
     </div>
   </div>
 </template>
@@ -23,19 +46,23 @@ export default {
   data() {
     return {
       IsShow: true,
+      VModelKey: "组件v-model",
+      TransitonShow: true,
     };
   },
   methods: {
+    changeValue() {
+      this.IsShow = !this.IsShow;
+    },
     GetChildren(e) {
       this.IsShow = e > 3;
-      console.log(e)
+      console.log(e);
+    },
+    Native() {
+      console.log("native是用来给自定义组件绑定原生事件时使用的修饰符");
     },
   },
-  mounted() {
-    setTimeout(() => {
-      this.IsShow = this.$children[0].HomeArray > 3;
-    }, 100);
-  },
+  mounted() {},
 };
 </script>
 
@@ -52,5 +79,12 @@ export default {
   justify-content: center;
   align-items: center;
   background-color: aquamarine;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
