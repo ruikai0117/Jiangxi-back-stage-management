6ff38cc041f7d3c8d05d25bdf6f25defa134463a
双面胶 <940165601@qq.com>
2021-05-25 15:07:13 +0800

    表单验证

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-15 09:58:55
- * @LastEditTime: 2021-04-15 14:31:50
+ * @LastEditTime: 2021-05-25 14:21:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /测试/Jiangxi-back-stage-management/src/router/index.js
@@ -19,6 +19,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: () => import('../view/Home/index')
+    },
+    {
+      path:'/WeCenter',
+      name:'WeCenter',
+      component:() => import('../view/WeCenter/inedx')
     }
   ]
 })
---
+++ src/view/WeCenter/inedx.vue
@@ -0,0 +1,84 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-05-25 14:19:50
+ * @LastEditTime: 2021-05-25 15:06:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /test-model/src/view/WeCenter/inedx.vue
+-->
+
+<template>
+  <div>
+    <el-form
+      :model="PersonalDetails"
+      ref="numberValidateForm"
+      label-width="100px"
+      class="demo-ruleForm"
+      :rules="PersonalDetailsRules"
+    >
+      <el-form-item label="姓名" prop="name">
+        <el-input
+          type="name"
+          v-model="PersonalDetails.name"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="爱好" prop="hobby">
+        <el-input
+          type="hobby"
+          v-model="PersonalDetails.hobby"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="年龄" prop="age">
+        <el-input
+          type="age"
+          v-model="PersonalDetails.age"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "WeCenter",
+  data() {
+    var TestName = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入您的姓名"));
+      } else if (value.length < 2 || value.length > 4) {
+        callback(new Error("请输入正确的姓名"));
+      }
+      callback();
+    };
+    var TestHobby = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入您的爱好"));
+      }
+      callback();
+    };
+    var TestAge = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入您的年龄"));
+      }
+      callback();
+    };
+    return {
+      PersonalDetails: {
+        name: "",
+        hobby: "",
+        age: "",
+      },
+      PersonalDetailsRules: {
+        name: [{ validator: TestName, trigger: "change" }],
+        hobby: [{ validator: TestHobby, trigger: "change" }],
+        age: [{ validator: TestAge, trigger: "change" }],
+      },
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
